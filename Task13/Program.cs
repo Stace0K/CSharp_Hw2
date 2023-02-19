c18a585f1f0defe093f63ce983d5b50977f268ea
anstice <ko.an.2018@yandex.ru>
2023-02-19 22:21:55 +0300

    task13

---
+++ Task13/Program.cs
@@ -0,0 +1,40 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//Ввод
+int D(string arg)
+{
+    Console.Write(arg);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+//Проверка на трехзначность и более
+bool Correct(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+//Возврат нужной цифры
+int ThirdN(int number)
+{
+    while (number > 1000)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+//Вывод
+int N = D("Введите число: ");
+{
+    if (Correct(N))
+    {
+        Console.WriteLine(ThirdN(N));
+    }
+}
